55e7cac5f0d232fd47ac7f7f7d4316292c94b786
charten <645070346@qq.com>
2018-07-27 18:37:34 +0800

    u

--- learnningWebSite/StringAnmt2/shaders/f.glsl
+++ learnningWebSite/StringAnmt2/shaders/f.glsl
@@ -7,11 +7,10 @@ varying vec2 v_position;
 
 void main(){
 	vec4 color = texture2D(u_tex1,v_position);
-	vec4 pxColor = texture2D(u_tex1,vec2(gl_FragCoord.x,u_resolution.y-gl_FragCoord.y)/u_resolution);
+	vec4 px_color = texture2D(u_tex1,vec2(gl_FragCoord.x,u_resolution.y-gl_FragCoord.y)/u_resolution);
 	float s = 1.0/u_len;
 	float gary = (color.r+color.g+color.b)/3.0;
-	// float gary = 1.0;
-	float p = float(int((1.0-gary)/s))*s;
+	float p = float(int((1.0-gary)/s)-1)*s;
 
 	vec4 text_color = texture2D(u_tex2,vec2(gl_PointCoord.x/u_len +p,gl_PointCoord.y));
 
@@ -20,7 +19,5 @@ void main(){
 	}
 
 
-	// gl_FragColor=vec4(vec3(gary),1.0);
 	gl_FragColor=text_color;
-	// gl_FragColor=pxColor;
 }
